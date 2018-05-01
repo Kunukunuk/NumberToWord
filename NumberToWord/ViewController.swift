3b71aa9cb52bb684cc122ebd74854403351dd4bd
Kun Huang <kkun.huang@gmail.com>
2018-05-01 18:08:45 -0400

    Initial Commit

---
+++ NumberToWord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NumberToWord
+//
+//  Created by Kun Huang on 5/1/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
